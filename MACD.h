588d302106dfb92810a19d81f035622fe55058a0
kebin33 <kgong@uchicago.edu>
2016-05-20 23:31:59 -0500

    Update MACD.h

--- MACD.h
+++ MACD.h
@@ -8,7 +8,7 @@ class MACD :public Indicator{
 public:
 	MACD(int n1=26, int n2=12, int m=9);
 	virtual int GetSignal(FuturePrice &p,vector<double>v);
-	virtual MACD* clone() const=0;
+	virtual MACD* clone();
 private:
 	int n1_;
 	int n2_;
