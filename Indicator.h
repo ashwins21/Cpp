d6ab46d94b35876979cf4ccffb80ffde4a2a1834
kebin33 <kgong@uchicago.edu>
2016-05-20 23:18:08 -0500

    Update Indicator.h

--- Indicator.h
+++ Indicator.h
@@ -1,9 +1,12 @@
 #pragma once
-#include<vector>
+//#include "FuturePrice.h"
+#include <vector> 
 using std::vector;
 
-class Indicator {
-protected:
-	int signal_;
+class Indicator
+{
+public:
+	virtual int GetSignal(FuturePrice p, vector<double>v) const=0;
+	virtual Indicator* clone() const = 0;
+	Indicator() {};
 };
-
