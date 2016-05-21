130e0da73e9ec6b9fe87e9eef333fc3ac8f5d02c
kebin33 <kgong@uchicago.edu>
2016-05-20 23:29:45 -0500

    Update MACD.h

--- MACD.h
+++ MACD.h
@@ -1,9 +1,17 @@
 #pragma once
-#include"Indicator.h"
-class MACD :public Indicator {
-public:
-	vector<int> getSignal;
-private:
+#include "Indicator.h"
+#include "FuturePrice.h"
+#include <vector>
+using std::vector;
 
+class MACD :public Indicator{
+public:
+	MACD(int n1=26, int n2=12, int m=9);
+	virtual int GetSignal(FuturePrice &p,vector<double>v);
+	virtual MACD* clone() const=0;
+private:
+	int n1_;
+	int n2_;
+	int m_;
 
 };
